17bc3daf66e8394a7e0e4d97c1d7e8abbd5234d6
Depriest43 <depriest43@gmail.com>
2023-11-15 16:30:19 -0800

    Finished Project for grade...

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,14 +1,16 @@
-﻿namespace MauiProjectGA1;
+﻿using Windows.UI.Accessibility;
+using System.Speech.Synthesis;
+
+namespace MauiProjectGA1;
 
 public partial class MainPage : ContentPage
 {
 	int count = 0;
+    SpeechSynthesizer speech = new SpeechSynthesizer();
 
-	public MainPage()
+    public MainPage()
 	{
 		InitializeComponent();
-
-
 	}
 
 
@@ -16,13 +18,44 @@ public partial class MainPage : ContentPage
 	private void btnAdd_Clicked(object sender, EventArgs e)
 	{
 		//Start writing code here.
-		doble.tryParse(txtNumber1.Text, out count);
-		double.TryParse(txtNumber2.Text, out count);
+		double number1 = double.Parse(txtNumber1.Text);
+
+		double number2 = double.Parse(txtNumber2.Text);
+
 		double result = number1 + number2;
-		lblResult.Text = $("Result: {result}");
+		lblResult.Text = ($"Result: {result}");
+
+		speech.Speak(lblResult.Text);
+		
+
 	}
 
+    private void btnSubtract_Clicked(object sender, EventArgs e)
+    {
+		double number1 = double.Parse(txtNumber1.Text);
+		double number2 = double.Parse(txtNumber2.Text);
+		double result = number1 - number2;
+		lblResult.Text = ($"Result: {result}");
+		speech.Speak(lblResult.Text);
+    }
 
-	}
+    private void btnMultiply_Clicked(object sender, EventArgs e)
+    {
+double number1 = double.Parse(txtNumber1.Text);
+		double number2 = double.Parse(txtNumber2.Text);
+		double result = number1 * number2;
+		lblResult.Text = ($"Result: {result}");
+		speech.Speak(lblResult.Text);
+    }
+
+    private void btnDivide_Clicked(object sender, EventArgs e)
+    {
+double number1 = Double.Parse(txtNumber1.Text);
+		double number2 = Double.Parse(txtNumber2.Text);
+		double result = number1 / number2;
+		lblResult.Text = ($"Result: {result}");
+		speech.Speak(lblResult.Text);
+    }
 }
+
 
